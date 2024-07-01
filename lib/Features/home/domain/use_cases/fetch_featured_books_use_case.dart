0d77b845192a767c11360e325031109b21d36229
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-01 19:50:54 +0300

    refactor to use case class and use generic use case

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,16 +1,23 @@
 import 'package:bookly_app/Core/errors/failure.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/no_param_use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends NoParamUseCase {
 
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async{
 
-    return homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchFeaturedBooks();
   }
+
 }
+
+
+
+
---
+++ lib/Features/home/domain/use_cases/use_case.dart
@@ -0,0 +1,6 @@
+import 'package:bookly_app/Core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, param> {
+  Future<Either<Failure,Type>> call(param param);
+}
