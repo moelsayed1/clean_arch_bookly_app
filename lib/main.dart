c44567d20fb4d4224b03c8974f0ea9e371e66c04
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-08 06:19:44 +0300

    edit hive in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,19 @@ import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 void main() async {
-  runApp(const BooklyApp());
+
+  await Hive.initFlutter();
   
   Hive.registerAdapter(BookEntityAdapter());
   
-  await Hive.openBox(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+
+  await Hive.openBox<BookEntity>(kNewestBox);
+
+  runApp(const BooklyApp());
 }
 
 class BooklyApp extends StatelessWidget {
