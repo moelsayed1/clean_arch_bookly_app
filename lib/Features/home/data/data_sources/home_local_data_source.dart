321ad4e56c9db42172a78f079a22e22ead6e2673
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-08 06:03:54 +0300

    fetch featured books and newest books local data source

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -1,4 +1,6 @@
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/constant.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
 
@@ -9,11 +11,18 @@ abstract class HomeLocalDataSource {
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    throw UnimplementedError();
+
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+
+    return box.values.toList();
   }
 
   @override
   List<BookEntity> fetchNewestBooks() {
-    throw UnimplementedError();
+
+
+    var box = Hive.box<BookEntity>(kNewestBox);
+
+    return box.values.toList();
   }
 }
