fe9186258ebf1d2530bb8d08740b74c8a2d2be06
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-09 06:44:00 +0300

    implement newest books cubit

--- lib/Features/home/presentation/manager/cubits/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/cubits/newest_books_cubit/newest_books_cubit.dart
@@ -1,11 +1,22 @@
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 import 'package:bookly_app/Features/home/presentation/manager/cubits/newest_books_cubit/newest_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
 
   Future<void> fetchNewestBooks() async {
 
     emit(NewestBooksLoading());
+
+    var result = await fetchNewestBooksUseCase.call();
+
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
   }
 }
