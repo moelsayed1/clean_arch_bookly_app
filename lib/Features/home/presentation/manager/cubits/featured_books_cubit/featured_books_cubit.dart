35f2b7638ba6789f7d47535b3ccd9720d251b1d2
moelsayed1 <mostafaembaby19@gmail.com>
2024-07-09 06:07:17 +0300

    create featured books states

---
+++ lib/Features/home/presentation/manager/cubits/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
---
+++ lib/Features/home/presentation/manager/cubits/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+
+class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+
+  final List<BookEntity> books;
+
+  FeaturedBooksSuccess(this.books);
+}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+
+  final String errMessage;
+
+  FeaturedBooksFailure(this.errMessage);
+}
